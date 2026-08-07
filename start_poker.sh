7bc7e016b800127ea050dfe9988d9eb032bc8f02
Haptic99 <daniel199898gmail.com>
2026-08-07 22:40:03 +0200

    Auto-deploy update

--- start_poker.sh
+++ start_poker.sh
@@ -11,5 +11,8 @@ python3 poker_server.py &
 # Warten, bis der Server-Port 8765 bereit ist
 sleep 2
 
+# XDG_RUNTIME_DIR setzen, damit SSH-Deployments Zugriff auf Wayland (GUI) haben
+export XDG_RUNTIME_DIR=/run/user/1000
+
 # Admin-Client auf dem Display anzeigen (XCB-Plattform für Wayland-Kompatibilität)
 QT_QPA_PLATFORM=xcb DISPLAY=:0 python3 poker_admin_client.py
